ee85c6499435f953dcf8adc7e004724486831ec2
Zac Lovoy <zwlovoy@gmail.com>
2014-11-12 23:48:34 +0000

    Added request

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var twit = require('twit');
+var request = require('request');
 
 var gifKey = process.env.GIPHYKEY;
 
